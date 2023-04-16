c56eb2e3ee7a888ab05663cd0d148d3a6a41ca05
dcampos <dcampos@users.noreply.github.com>
2023-04-16 16:58:09 -0300

    Update workflow

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update
+sudo apt-get install -y devscripts equivs
+
+PKGVERSION=$(dpkg-parsechangelog --show-field=Version)
+NAME=$(dpkg-parsechangelog --show-field=Source)
+VERSION=${PKGVERSION/-*}
+RELEASE=${PKGVERSION/*-}
+NAME="neovim"
+
+echo "Create build directory"
+rm -Rf build
+mkdir build
+cd build
+
+wget --progress=dot:mega \
+    https://github.com/"${NAME}"/"${NAME}"/archive/refs/tags/v"${VERSION}".tar.gz
+
+echo "Prepare sources"
+mv v"${VERSION}".tar.gz "${NAME}"_"${VERSION}".orig.tar.gz
+tar xf "${NAME}"_"${VERSION}".orig.tar.gz
+cd "${NAME}-${VERSION}"
+
+cp -R ../../debian .
+
+sudo mk-build-deps --install --remove \
+    --tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes" \
+    debian/control
+
+dpkg-buildpackage -us -uc -b
